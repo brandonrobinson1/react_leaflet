e17c7b3fa6bbc170a6d600cddd7a02459dd6bd06
brobinson21 <145131862+brobinson21@users.noreply.github.com>
2024-01-26 13:14:43 -0500

    initialize map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer } from "react-leaflet";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={[43, -75]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* WATERCOLOR CUSTOM TILES */}
+      {/* <TileLayer
+    attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
+  /> */}
+      {/* GOOGLE MAPS TILES */}
+      {/* <TileLayer
+    attribution="Google Maps"
+    // url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
+    // url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
+    url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
+    maxZoom={20}
+    subdomains={["mt0", "mt1", "mt2", "mt3"]}
+  /> */}
+    </MapContainer>
   );
 }
 
