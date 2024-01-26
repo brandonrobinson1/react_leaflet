a1c8f5ee7242ab8af2b21b26d2618f7c7f85a8a6
brobinson21 <brandonrobinson0000@gmail.com>
2024-01-26 15:51:34 -0500

    add click to find self button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
+import { Icon } from "leaflet";
+import { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+
 function App() {
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+
+    return position === null ? null : (
+      <Marker icon={customIcon} position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+  }
+  const customIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
+    // iconUrl: require("./icons/Map_pin_icon"),
+    iconSize: [38, 38], // size of the icon
+  });
+
   return (
-    <MapContainer center={[43, -75]} zoom={13}>
+    <MapContainer center={[40.730761, -73.935242]} zoom={13}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png
+      "
       />
-      {/* WATERCOLOR CUSTOM TILES */}
-      {/* <TileLayer
-    attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
-  /> */}
-      {/* GOOGLE MAPS TILES */}
-      {/* <TileLayer
-    attribution="Google Maps"
-    // url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
-    // url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
-    url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
-    maxZoom={20}
-    subdomains={["mt0", "mt1", "mt2", "mt3"]}
-  /> */}
+      <LocationMarker />
     </MapContainer>
   );
 }
 
 export default App;
+//  <Marker
+//         position={[40.78617, -73.94197]}
+//         icon={customIcon}
+//         draggable={true}
+//       >
+//         <Popup>
+//           A pretty CSS3 popup. <br /> Easily customizable.
+//         </Popup>
+//       </Marker>
